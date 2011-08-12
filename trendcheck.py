4bb780811ce34c88bdb7bba359d5cbf3fc5001b4
Stephen Donner <stephen.donner@gmail.com>
2011-08-12 02:25:37 -0700

    Small bits of cleanup
    Should be self-evident and pretty cut-and-dry

--- trendcheck.py
+++ trendcheck.py
@@ -4,23 +4,23 @@ import re
 
 
 def testAnalyticsTags(url):
-    """ Check that webtrends image is present on page"""
+    """ Check that Webtrends image is present on page"""
     page = pq(url)
-    ##regular expression for webtrends tags
+    # regular expression for Webtrends tags
     dcsid = re.search('[dcs]{3}[a-z,0-9_]{27}', str(page))
     tag = page('img#DCSIMG')
     print type(tag.attr('src'))
     if tag.attr('src') != str and dcsid == None:
         print "Error: " + url + " has no Webtrends tag"
-        raise Exception("Webtrend Tag not found")
+        raise Exception("Webtrends tag not found")
     else:
-        print "Url: " + url
+        print "URL: " + url
         print "Tag Link: " + tag.attr('src')
         print "Tag:" + dcsid.group()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Script to check for webtrends tags")
+        description="Script to check for Webtrends tags")
     parser.add_argument('--file', action="store",
         dest='file', help='filename', type=str)
     parser.add_argument('--url', action="store", type=str,
@@ -35,4 +35,4 @@ if __name__ == "__main__":
         for url in urls:
             testAnalyticsTags(url)
     else:
-        print "Enter a valid  filename or url"
+        print "Please enter a valid filename or URL"
